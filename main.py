90a1a00ebc4f664069a7bbe6662404f3379d2eaf
jocarrojas <97867812+jocarrojas@users.noreply.github.com>
2023-06-07 17:20:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# String manipulation. capitalize, lower, strip
+names = []
+surnames = []
+name = input("Add a name\n").strip().capitalize()
+names.append(name)
+print()
+surname = input("Add a surname\n").strip().capitalize()
+surnames.append(surname)
+fullName = (f"{name} {surname}")
+fullNames = []
+if fullName not in fullNames:
+  fullNames.append(f"{name} {surname}")
+else:
+  print("ERROR: Duplicate name")
+for i in range(0,len(fullNames)):
+  print(f"{i+1}. {fullNames[i]}")
